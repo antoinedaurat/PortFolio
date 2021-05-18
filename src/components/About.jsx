bf173b046e3ed109798258f630e95d785d11bef9
Antoine Daurat <antoinedaurat@gmail.com>
2021-05-18 09:43:39 +0200

    small cleanups

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,13 @@ export const About = () => (
             <h1 className="title has-text-centered">You found me!</h1>
 
             <p>
-                I'm Antoine. Born and raised in Paris, I studied Music Composition in Berlin where I became Machine
-                Learning &
-                Full Stack Web Developer around 2018.
+                I'm Antoine. Born and raised in Paris, I studied Music Composition in Berlin where I became
+                <span className="has-text-weight-bold"> Machine Learning & Full Stack Web Developer </span>
+                around 2018.
             </p>
             <br/>
             <p>Here, you'll get a quick sense of where I come from, what I am doing now and what I generally can do.</p>
+            <br/>
             <p>For more details, take a look at <a href={escape('/CV Developer 05.2021.pdf')}>my current CV</a>.</p>
         </div>
     </div>
